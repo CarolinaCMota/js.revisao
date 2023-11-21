98c2a1fb7b993e48135226b3ec39f03afdff1155
CarolinaCMota <117762777+CarolinaCMota@users.noreply.github.com>
2023-11-21 15:45:40 -0300

    Add files via upload

---
+++ Desafio Intensivao/main.js
@@ -0,0 +1,101 @@
+alert("Hello World!");
+
+//allert com mensagem eh so escrever alert e a mensagem, bem simples.
+
+var numero1 = 5;
+var numero2 = 10;
+
+// Soma das duas variáveis
+
+/*
+primeiro apresenta as variaveis, 
+let resulltado= n+ n2;
+*/
+var resultado = numero1 + numero2;
+
+// Exibição do resultado em um alerta
+alert("A soma de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+
+var minhaVariavel = 42;
+
+// Verificação do tipo de dado usando typeof
+if (typeof minhaVariavel === 'number') {
+  // Se for um número
+  alert("É um número");
+} else {
+  // Se não for um número
+  alert("Não é um número");
+}
+
+
+var minhaVariavel = "Olá, mundo!";
+
+  // Verificação do tipo de dado usando typeof
+  if (typeof minhaVariavel === 'string') {
+    // Se for uma string
+    alert("É uma string");
+  } else {
+    // Se não for uma string
+    alert("Não é uma string");
+  }
+
+  var minhaVariavel = true;
+
+  // Verificação do tipo de dado usando typeof
+  if (typeof minhaVariavel === 'boolean') {
+    // Se for um booleano
+    alert("É um booleano");
+  } else {
+    // Se não for um booleano
+    alert("Não é um booleano");
+  }
+
+  var numero1 = 15;
+  var numero2 = 7;
+
+  // Subtração das duas variáveis
+  var resultado = numero1 - numero2;
+
+  // Exibição do resultado em um alerta
+  alert("A subtração de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+
+  var numero1 = 5;
+  var numero2 = 8;
+
+  // Multiplicação das duas variáveis
+  var resultado = numero1 * numero2;
+
+  // Exibição do resultado em um alerta
+  alert("A multiplicação de " + numero1 + " e " + numero2 + " é igual a " + resultado);
+
+
+  var numero1 = 20;
+  var numero2 = 4;
+
+  // Divisão das duas variáveis
+  var resultado = numero1 / numero2;
+
+  // Exibição do resultado em um alerta
+  alert("A divisão de " + numero1 + " por " + numero2 + " é igual a " + resultado);
+
+  var meuNumero = 8;
+
+  // Verificação se o número é par
+  if (meuNumero % 2 === 0) {
+    // Se for um número par
+    alert("É um número par");
+  } else {
+    // Se não for um número par
+    alert("Não é um número par");
+  }
+
+  var meuNumero = 7;
+
+  // Verificação se o número é ímpar
+  if (meuNumero % 2 !== 0) {
+    // Se for um número ímpar
+    alert("É um número ímpar");
+  } else {
+    // Se não for um número ímpar
+    alert("Não é um número ímpar");
+  }
